c75d4b0fb5f10ce7a813a6bb0ac383ba975f59e0
Raj <raj1992_patel@windowslive.com>
2020-02-20 03:57:16 +1300

    Initial Commit

---
+++ Swann Video StreamingTests/Swann_Video_StreamingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swann_Video_StreamingTests.swift
+//  Swann Video StreamingTests
+//
+//  Created by Raj Patel on 20/02/20.
+//  Copyright © 2020 Raj Patel. All rights reserved.
+//
+
+import XCTest
+@testable import Swann_Video_Streaming
+
+class Swann_Video_StreamingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
