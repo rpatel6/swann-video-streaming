d6b07ef453b0ca69d222c99e70449158aaf42cc9
Raj <raj1992_patel@windowslive.com>
2020-03-06 22:30:33 +1300

    replaced segmented control with uipageviewcontroller

---
+++ Swann Video Streaming/ScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ScreenViewController.swift
+//  Swann Video Streaming
+//
+//  Created by Raj Patel on 6/03/20.
+//  Copyright © 2020 Raj Patel. All rights reserved.
+//
+
+import Foundation
